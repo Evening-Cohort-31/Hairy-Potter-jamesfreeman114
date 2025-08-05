3fd859fcf459732b707110be06d4a96ec6a1c56b
James Freeman <jamesfreeman114@gmail.com>
2025-08-04 20:39:40 -0500

    Added Kiln Module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 // Imports go first
 
-import { makePottery } from "./potteryWheel.js"
+
+import { makePottery } from './potteryWheel.js'
+import { firePottery } from './kiln.js'
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("Mug", 7, 5)
@@ -21,6 +23,22 @@ console.log(pitcher)
 
 
 // Fire each piece of pottery in the kiln
+
+let firedMug = firePottery(mug, 2500)
+console.log(firedMug)
+
+let firedVase = firePottery(vase, 2000)
+console.log(firedVase)
+
+let firedBowl = firePottery(bowl, 2100)
+console.log(firedBowl)
+
+let firedPlatter = firePottery(platter, 1800)
+console.log(firedPlatter)
+
+let firedPitcher = firePottery(pitcher, 2199)
+console.log(firedPitcher)
+
 
 
 // Determine which ones should be sold, and their price
